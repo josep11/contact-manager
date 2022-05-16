b620ca2cf45a8df60986b073e8fbd140975bb7bc
Josep Alsina <josepalsinasio@gmail.com>
2022-05-16 11:39:12 +0200

    upgraded virtualenv to 3.10.4

---
+++ example.py
@@ -0,0 +1,17 @@
+from tkinter import Tk
+import tkinter.ttk as ttk
+
+# Create an instance of Tkinter Frame
+win = Tk()
+
+# Set the geometry
+win.geometry("700x350")
+
+# Set the default color of the window
+win.config(background="#24f3f0")
+win.config(bg="SkyBlue1")
+win.configure(background="white")
+
+ttk.Label(win, text="Hey There! Welcome to TutorialsPoint", font=("Helvetica 22 bold"), foreground="navy").pack()
+
+win.mainloop()
