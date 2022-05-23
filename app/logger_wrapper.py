1b6d336c4b71e13d69e814447dd44c12bbce1ae8
Josep Alsina <josepalsinasio@gmail.com>
2022-05-23 13:33:28 +0200

    preparing to tail logs

--- app/logger_wrapper.py
+++ app/logger_wrapper.py
@@ -11,13 +11,14 @@ logger.setLevel(logging.INFO)
 # -------- BEGIN CONFIG ---------- #
 # -------------------------------- #
 DEBUG_LOG_FILENAME = "debug.log"
-PROD_LOG_FILENAME = "editpasteapp.log"
 # -------------------------------- #
 # --------- END CONFIG ----------- #
 # -------------------------------- #
 
 # https://docs.python.org/3/howto/logging.html
-log_file_dir = home + f"/Library/Logs/{AppConfig.APP_NAME}/"  # create logs directory if not exists
+# create logs directory if not exists
+PROD_LOG_FILENAME = f"{AppConfig.APP_NAME.lower()}.log"
+log_file_dir = home + f"/Library/Logs/{AppConfig.APP_NAME}/"
 log_file = log_file_dir + PROD_LOG_FILENAME
 try:
     os.makedirs(log_file_dir)
@@ -25,7 +26,8 @@ except FileExistsError as e:
     pass
 
 if AppConfig.isDev:
-    logging.basicConfig(filename=DEBUG_LOG_FILENAME, encoding="utf-8", level=logging.DEBUG)
+    logging.basicConfig(filename=DEBUG_LOG_FILENAME,
+                        encoding="utf-8", level=logging.DEBUG)
 else:
     logging.basicConfig(
         filename=log_file, encoding="utf-8", level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
